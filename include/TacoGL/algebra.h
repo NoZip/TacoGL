cc0934f34af875980485bdaa4d87a197913b75a8
Thomas 'NoZip' Pizon <pizon.thomas@gmail.com>
2014-09-23 12:16:42 -0400

    Little corrections

--- include/TacoGL/algebra.h
+++ include/TacoGL/algebra.h
@@ -1,5 +1,5 @@
-#ifndef __GL_ALGEBRA__
-#define __GL_ALGEBRA__
+#ifndef __TACOGL_ALGEBRA__
+#define __TACOGL_ALGEBRA__
 
 #include <Morpheus/Vector.h>
 #include <Morpheus/Matrix.h>
@@ -9,26 +9,29 @@
 namespace TacoGL
 {
 
-using Vector2 = Morpheus::Vector<gl::GLfloat, 2>;
-using Vector2i = Morpheus::Vector<gl::GLint, 2>;
-using Vector2ui = Morpheus::Vector<gl::GLuint, 2>;
-using Vector2b = Morpheus::Vector<gl::GLboolean, 2>;
+  using Vector2 = Morpheus::Vector<gl::GLfloat, 2>;
+  using Vector2i = Morpheus::Vector<gl::GLint, 2>;
+  using Vector2ui = Morpheus::Vector<gl::GLuint, 2>;
+  using Vector2b = Morpheus::Vector<gl::GLboolean, 2>;
 
-using Vector3 = Morpheus::Vector<gl::GLfloat, 3>;
-using Vector3i = Morpheus::Vector<gl::GLint, 3>;
-using Vector3ui = Morpheus::Vector<gl::GLuint, 3>;
-using Vector32b = Morpheus::Vector<gl::GLboolean, 3>;
+  using Vector3 = Morpheus::Vector<gl::GLfloat, 3>;
+  using Vector3i = Morpheus::Vector<gl::GLint, 3>;
+  using Vector3ui = Morpheus::Vector<gl::GLuint, 3>;
+  using Vector3b = Morpheus::Vector<gl::GLboolean, 3>;
 
-using Vector4 = Morpheus::Vector<gl::GLfloat, 4>;
-using Vector4i = Morpheus::Vector<gl::GLint, 4>;
-using Vector4ui = Morpheus::Vector<gl::GLuint, 4>;
-using Vector4b = Morpheus::Vector<gl::GLboolean, 4>;
+  using Vector4 = Morpheus::Vector<gl::GLfloat, 4>;
+  using Vector4i = Morpheus::Vector<gl::GLint, 4>;
+  using Vector4ui = Morpheus::Vector<gl::GLuint, 4>;
+  using Vector4b = Morpheus::Vector<gl::GLboolean, 4>;
 
+  using Matrix2 = Morpheus::Matrix<gl::GLfloat, 2>;
+  using Matrix3 = Morpheus::Matrix<gl::GLfloat, 3>;
+  using Matrix4 = Morpheus::Matrix<gl::GLfloat, 4>;
 
-using Matrix2 = Morpheus::Matrix<gl::GLfloat, 2>;
-using Matrix3 = Morpheus::Matrix<gl::GLfloat, 3>;
-using Matrix4 = Morpheus::Matrix<gl::GLfloat, 4>;
+  using Matrix2d = Morpheus::Matrix<gl::GLdouble, 2>;
+  using Matrix3d = Morpheus::Matrix<gl::GLdouble, 3>;
+  using Matrix4d = Morpheus::Matrix<gl::GLdouble, 4>;
 
-} // end namespace GL
+} // end namespace TacoGL
 
 #endif
