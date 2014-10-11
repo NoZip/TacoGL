04d6646cb08b7148e229874c11d65a2c3be2a2a9
Thomas 'NoZip' Pizon <pizon.thomas@gmail.com>
2014-10-11 19:07:54 -0400

    Convolve shader compiles now

--- shaders/convolve.compute.glsl
+++ shaders/convolve.compute.glsl
@@ -1,124 +1,69 @@
 #version 430 core
-#extension ARB_shader_image_load_store : require
-#extension ARB_shader_subroutine       : require
+#extension GL_ARB_shader_image_load_store : require
 
-layout(local_size_x = 32, local_size_y = 32) in; // max size 1024 = 32*32
+#ifndef WORKGROUP_SIZE_X
+#define WORKGROUP_SIZE_X 32
+#endif
 
-subroutine void Convolve(
-  ivec2 index,
-  float kernel[],
-  int halfSize,
-  image2D src,
-  image2D dst
-);
+#ifndef WORKGROUP_SIZE_Y
+#define WORKGROUP_SIZE_Y 32
+#endif
 
-uniform readonly image2D src;
-uniform writeonly image2D dst;
+#ifndef KERNEL_HALFSIZE
+#define KERNEL_HALFSIZE 1
+#endif
 
-uniform int kernelHalfSize;
+#define KERNEL_SIZE (2*KERNEL_HALFSIZE + 1)
 
-subroutine uniform Convolve convolve;
+#ifndef GREYSCALE
+#define FORMAT rgba8
+#else
+#define FORMAT r8
+#endif
 
-/**
- * Defined in row-major order.
- * Here is an example for a 2D kernel size of 3:
- * 
- * |===|===|===|
- * | 0 | 1 | 2 |
- * |---|---|---|
- * | 3 | 4 | 5 |
- * |---|---|---|
- * | 6 | 7 | 8 |
- * |===|===|===|
- *
- * 1D kernels are simplier:
- * 
- * |===|===|===|
- * | 0 | 1 | 2 |
- * |===|===|===|
- */
-buffer float kernel[];
+
+layout(local_size_x = WORKGROUP_SIZE_X, local_size_y = WORKGROUP_SIZE_Y) in;
+
+uniform uvec2 imageSize;
+
+layout(FORMAT, binding = 0) uniform image2D src;
+layout(FORMAT, binding = 1) uniform image2D dst;
+
+uniform float kernel[KERNEL_SIZE*KERNEL_SIZE];
 
 /**
  * Fetches a value in a 2D array.
  * @param index The index to fetch.
  */
-float at(float kernel[], ivec2 index)
+float at(float kernel[KERNEL_SIZE*KERNEL_SIZE], ivec2 index)
 {
-  return kernel[index.x*kernelSize + index.y];
-}
-
-subroutine(Convolve)
-void convolve1DH(
-  ivec2 index,
-  float kernel[],
-  int halfSize,
-  image2D src,
-  image2D dst
-)
-{
-  vec4 value = 0;
-
-  for (int n = -halfSize, n <= halfSize; ++n)
-  {
-    value += kernal[n + halfSize] * imageLoad(src, index + ivec2(n, 0));
-  }
-
-  imageStore(dst, index);
-}
-
-subroutine(Convolve)
-void convolve1DV(
-  ivec2 index,
-  float kernel[],
-  int halfSize,
-  image2D src,
-  image2D dst
-)
-{
-  vec4 value = 0;
-
-  for (int n = -halfSize, n <= halfSize; ++n)
-  {
-    value += kernel[n + halfSize] * imageLoad(src, index + ivec2(0, n));
-  }
-
-  imageStore(dst, index, value);
-}
-
-subroutine(Convolve)
-void convolve2D(
-  ivec2 index,
-  float kernel[],
-  int halfSize,
-  image2D src,
-  image2D dst
-)
-{
-  vec4 value = 0;
-
-  for (int i = -halfSize; i <= halfSize; ++i)
-  {
-    for (int J = -halfSize; i <= halfSize; ++j)
-    {
-      float coeff = at(kernel, ivec2(i + halfSize, j + halfsize));
-      value += coeff * imageLoad(src, index + ivec2(i, j));
-    }
-  }
-
-  imageStore(dst, index, value);
+  return kernel[index.x*3 + index.y];
 }
 
 void main()
 {
-  ivec2 index = gl_GlobalInvocationID.xy;
-  ivec2 size = imageSize(dst);
+  uvec2 index = gl_GlobalInvocationID.xy;
 
   // checks if the index is in the image.
-  if (index.x > size.x || index.y > size.y)
+  if (index.x > imageSize.x || index.y > imageSize.y)
   {
     return;
   }
 
-  convolve(index, kernel, kernelHalfSize, src, dst);
+  vec4 value = vec4(0);
+
+  for (int i = -KERNEL_HALFSIZE; i <= KERNEL_HALFSIZE; ++i)
+  {
+    for (int j = -KERNEL_HALFSIZE; j <= KERNEL_HALFSIZE; ++j)
+    {
+      ivec2 kernelOffset = ivec2(i + KERNEL_HALFSIZE, j + KERNEL_HALFSIZE);
+      float coeff = kernel[kernelOffset.x*KERNEL_SIZE + kernelOffset.y];
+      ivec2 imageOffset = ivec2(i, j);
+      value += coeff * imageLoad(src, ivec2(index) + imageOffset);
+    }
+  }
+
+  clamp(value, 0, 1);
+
+  imageStore(dst, ivec2(index), value);
 }
